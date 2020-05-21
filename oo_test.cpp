1d06248927f3cecf898939f767e6b36e37285ba8
howard0027 <62835719+howard0027@users.noreply.github.com>
2020-05-21 20:02:29 +0800

    Create oo_test.cpp

---
+++ oo_test.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+
+class Player{
+  std::string name;
+  int money;
+  Player(name, money) : name(name), money(money) {}
+ };
+ 
+ int main()
+ {
+    Player John("John", 90);
+    return 0;
+ }
